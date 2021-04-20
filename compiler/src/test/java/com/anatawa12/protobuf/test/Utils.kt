2bf4e04e7ecb7903a3ea9b67eb0ac1a8b188ea4b
anatawa12 <anatawa12@icloud.com>
2021-04-20 14:00:03 +0900

    improve test fail log

--- compiler/src/test/java/com/anatawa12/protobuf/test/Utils.kt
+++ compiler/src/test/java/com/anatawa12/protobuf/test/Utils.kt
@@ -3,12 +3,14 @@ package com.anatawa12.protobuf.test
 private val hexArray = "0123456789ABCDEF".toCharArray()
 
 fun bytesToHex(bytes: ByteArray): String {
-    val hexChars = CharArray(bytes.size * 2)
+    val hexChars = CharArray(bytes.size * 2 + bytes.size / 8)
+    var i = 0
     for (j in bytes.indices) {
         val v = bytes[j].toInt() and 0xFF
 
-        hexChars[j * 2] = hexArray[v ushr 4]
-        hexChars[j * 2 + 1] = hexArray[v and 0x0F]
+        hexChars[i++] = hexArray[v ushr 4]
+        hexChars[i++] = hexArray[v and 0x0F]
+        if (j % 8 == 7) hexChars[i++] = ' '
     }
     return String(hexChars)
 }
